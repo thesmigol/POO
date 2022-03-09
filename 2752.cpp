0259cc83c84ef1d9140f09979cd2b110d67b1d55
Alvaro Tavares <alvaro_tavares@hotmail.com>
2022-03-09 15:33:30 -0300

    ai sim

--- 2752.cpp
+++ 2752.cpp
@@ -7,8 +7,15 @@ using namespace std;
 int main() {
 
 string frase = "AMO FAZER EXERCICIO NO URI";
+
 cout << '<' + frase + '>' << endl;
 cout << '<' << setw(30) << setfill(' ') << frase << '>' << endl;
 cout << '<' + frase.substr(0,20) + '>' << endl;
+cout << '<' + frase + '>' << endl;
+cout << '<' << left << setw(30) << setfill(' ') << frase << '>' << endl;
+cout << '<' + frase + '>' << endl;
+cout << '<' << "          AMO FAZER EXERCICIO " << '>' << endl;
+cout << '<' << "AMO FAZER EXERCICIO           " << '>' << endl;
+
     return 0;
 }
