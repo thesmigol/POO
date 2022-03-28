7f9586342370317b9be3f8428127b1b394aa2fb9
Alvaro Tavares <alvaro_tavares@hotmail.com>
2022-03-28 08:57:37 -0300

    Create 1059.cpp

---
+++ 1059.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <iomanip>
+#include <math.h>
+#include <string>
+using namespace std;
+
+
+int
+main ()
+{
+
+  int ENTRADA = 100, i;
+  for (i = 2; i <= ENTRADA; i = i + 2)
+    {
+      cout << i << endl;
+    }
+
+  return 0;
+}
